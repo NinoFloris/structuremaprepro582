d0a0e3982c9b3ec529756fdd3b5b998af1ea5938
Nino Floris <mail@ninofloris.com>
2017-06-27 22:16:07 +0200

    Got it 🎉

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using StructureMap;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace Repro
+{
+    public class Dependency
+    {
+        private static Func<int, int> Identity { get; } = f => f;
+        public Dependency(object param) { } // We need at least two constructors to get the desired failure
+        public Dependency(string param) { }
+    }
+
+    class Program
+    {
+        private static Container Container { get; } = new Container();
+
+        static void Main(string[] args)
+        {
+            Container.Populate(Enumerable.Empty<ServiceDescriptor>()); // Commenting out this line stops the exception
+            Container.GetInstance<Dependency>();
+        }
+    }
+}
